f05c982dc5b190694e37c38675c3c6cbaf24f7ec
Dibyendu Dhauri <dhauridibyendu@gmail.com>
2023-10-04 20:02:35 +0530

    added card details page with validation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import DetailsForm from "./components/DetailsForm";
 
 function App() {
+
+  const [cardDetails, setCardDetails] = useState({
+    cardHolderName: "",
+    cardNumber: "",
+    expiryMonth: "",
+    expiryYear: "",
+    cvc:""
+  })
+  
+  const [errorDetails, setErrorDetails] = useState({
+    cardHolderNameError: false,
+    cardNumberError: false,
+    expiryMonthError: false,
+    expiryYearError:false,
+    cvcError:false
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex ">
+      <div className="w-1/4 h-screen gradient-bg" />
+      <div className="w-3/4 bg-gray-300">
+        <DetailsForm cardDetails={cardDetails} setCardDetails={setCardDetails} 
+        setErrorDetails={setErrorDetails}
+        errorDetails={errorDetails} />
+      </div>
     </div>
   );
 }
---
+++ src/components/DetailsForm.jsx
@@ -0,0 +1,203 @@
+import React from "react";
+
+export default function DetailsForm({
+  cardDetails,
+  setCardDetails,
+  errorDetails,
+  setErrorDetails,
+}) {
+  const validateCardNumber = () => {
+    if (
+      !/^[0-9]+$/.test(cardDetails.cardNumber) ||
+      cardDetails.cardNumber.length !== 12
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const validateCardHolderName = () => {
+    if (!/^[a-zA-Z\s]+$/.test(cardDetails.cardHolderName)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const validateMonth = () => {
+    if (
+      !/^[0-9]+$/.test(cardDetails.expiryMonth) ||
+      cardDetails.expiryMonth.length !== 2
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  const validateYear = () => {
+    if (
+      !/^[0-9]+$/.test(cardDetails.expiryYear) ||
+      cardDetails.expiryYear.length !== 2
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const validateCvc = () => {
+    if (!/^[0-9]+$/.test(cardDetails.cvc) || cardDetails.cvc.length !== 3) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const cardHolderNameError = validateCardHolderName();
+    const cardNumberError = validateCardNumber();
+    const expiryMonthError = validateMonth();
+    const expiryYearError = validateYear();
+    const cvcError = validateCvc();
+    setErrorDetails({
+      cardHolderNameError: cardHolderNameError,
+      cardNumberError: cardNumberError,
+      expiryMonthError: expiryMonthError,
+      expiryYearError: expiryYearError,
+      cvcError: cvcError,
+    });
+  };
+
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className=" p-8 rounded-lg shadow-lg">
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className=" text-gray-700 font-bold">CARDHOLDER NAME</label>
+            <input
+              type="text"
+              className="form-input w-full mt-1 border border-gray-900 p-2 outline-none rounded-lg
+              bg-transparent "
+              placeholder="e.g. John Doe"
+              // required
+              value={cardDetails.cardHolderName}
+              onChange={(e) => {
+                setCardDetails({
+                  ...cardDetails,
+                  cardHolderName: e.target.value,
+                });
+              }}
+            />
+            {errorDetails.cardHolderNameError && (
+              <div className="text-red-600">
+                'Cardholder name should contain only letters and spaces'
+              </div>
+            )}
+          </div>
+
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 font-bold"
+            >
+              CARD NUMBER
+            </label>
+            <input
+              type="text"
+              className="form-input w-full mt-1 border border-gray-900 p-2 outline-none rounded-lg
+              bg-transparent "
+              placeholder="e.g. 1234 5678 9012 3456"
+              // required
+              value={cardDetails.cardNumber}
+              onChange={(e) => {
+                setCardDetails({
+                  ...cardDetails,
+                  cardNumber: e.target.value,
+                });
+              }}
+            />
+            {errorDetails.cardNumberError && (
+              <div className="text-red-600">
+                'Card Number should be numeric'
+              </div>
+            )}
+          </div>
+
+          <div className="flex mb-4">
+            <div className="">
+              <label className="block text-gray-700 font-bold ">
+                EXP. DATE (MM/YY)
+              </label>
+              <div className="flex">
+                <input
+                  type="text"
+                  className="form-input w-1/3 mt-1 mr-2 border border-gray-900 p-2 outline-none rounded-lg
+                bg-transparent "
+                  placeholder="MM"
+                  //   required
+                  value={cardDetails.expiryMonth}
+                  onChange={(e) =>
+                    setCardDetails({
+                      ...cardDetails,
+                      expiryMonth: e.target.value,
+                    })
+                  }
+                />
+
+                <input
+                  type="text"
+                  className="form-input w-1/3 mt-1 border border-gray-900 p-2 outline-none rounded-lg
+                bg-transparent "
+                  placeholder="YY"
+                  //   required
+                  value={cardDetails.expiryYear}
+                  onChange={(e) =>
+                    setCardDetails({
+                      ...cardDetails,
+                      expiryYear: e.target.value,
+                    })
+                  }
+                />
+              </div>
+
+              {(errorDetails.expiryMonthError || errorDetails.expiryYearError) && (
+                <div className="text-red-600">
+                  'Exp. Date should be Numeric'
+                </div>
+              )}
+            </div>
+
+            <div className="">
+              <label className="block text-gray-700 font-bold">CVC</label>
+              <input
+                type="text"
+                className="form-input  mt-1 border border-gray-900 p-2 outline-none rounded-lg
+              bg-transparent "
+                placeholder="e.g. 123"
+                //   required
+                value={cardDetails.cvc}
+                onChange={(e) => {
+                  setCardDetails({
+                    ...cardDetails,
+                    cvc: e.target.value,
+                  });
+                }}
+              />
+              {errorDetails.cvcError && (
+                <div className="text-red-600">'CVC should be Numeric'</div>
+              )}
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            className="bg-violet-900 text-white px-4 py-3 rounded-md w-full"
+          >
+            Confirm
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
